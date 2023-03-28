64532dc9a2d616dd81175793d75bbf48ce1dcd1c
victorog17 <victor.og17@gmail.com>
2023-03-27 21:36:04 -0300

    update

--- A002_Fundamentos_Ingestao_Dados/main.py
+++ A002_Fundamentos_Ingestao_Dados/main.py
@@ -1,12 +1,37 @@
 #%%
 # imports
 import requests
+import json
 import pandas
 # %%
-print("Fera!")
+url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
+ret = requests.get(url)
 # %%
-a = 45
-b = 'victor'
-c = True
-d = 30.7
+if ret:
+    print(ret)
+else:
+    print('Falhou')
+# %%
+dolar = json.loads(ret.text)['USDBRL']
+# %%
+print(f"20 Dólares hoje custam {float(dolar['bid']) * 20:.2f} reais")
+# %%
+def cotacao(valor, moeda):
+    url = f'https://economia.awesomeapi.com.br/last/{moeda}'
+    ret = requests.get(url)
+    dolar = json.loads(ret.text)[moeda.replace('-','')]
+    print(f"{valor} {moeda[:3]} hoje custam {float(dolar['bid']) * valor:.2f} {moeda[-3:]}")
+
+#%%
+cotacao(20, 'USD-BRL')
+# %%
+cotacao(20, 'JPY-BRL')
+# %%
+lst_money = [
+        "USD-BRL",
+        "EUR-BRL",
+        "BTC-BRL",
+        "JPY-BRL",
+        "RPL-BRL"
+    ]
 # %%
