51655b135d9efa34f15a4590cea2040b0ec79bb7
victorog17 <victor.og17@gmail.com>
2023-03-28 08:46:18 -0300

    update

--- A002_Fundamentos_Ingestao_Dados/main.py
+++ A002_Fundamentos_Ingestao_Dados/main.py
@@ -2,7 +2,8 @@
 # imports
 import requests
 import json
-import pandas
+import backoff
+import random
 # %%
 url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
 ret = requests.get(url)
@@ -34,4 +35,46 @@ lst_money = [
         "JPY-BRL",
         "RPL-BRL"
     ]
+# %%
+def multi_moeda(valor, moeda):
+    url = f'https://economia.awesomeapi.com.br/last/{moeda}'
+    ret = requests.get(url)
+    dolar = json.loads(ret.text)[moeda.replace('-','')]
+    print(f"{valor} {moeda[:3]} hoje custam {float(dolar['bid']) * valor:.2f} {moeda[-3:]}")
+# %%
+def error_check(func):
+    def inner_func(*args, **kargs):
+        try:
+            func(*args, **kargs)
+        except:
+            print(f"{func.__name__} falhou")
+    return inner_func
+# %%
+@error_check
+def multi_moeda(valor, moeda):
+    url = f'https://economia.awesomeapi.com.br/last/{moeda}'
+    ret = requests.get(url)
+    dolar = json.loads(ret.text)[moeda.replace('-','')]
+    print(f"{valor} {moeda[:3]} hoje custam {float(dolar['bid']) * valor:.2f} {moeda[-3:]}")
+# %%
+multi_moeda(50, "EUR-BRL")
+# %%
+@backoff.on_exception(backoff.expo,(ConnectionAbortedError, ConnectionRefusedError, TimeoutError), max_tries=10)
+def test_func(*args, **kargs):
+    rnd = random.random()
+    print(f"""
+            RND: {rnd}
+            args: {args if args else 'sem args'}
+            kargs: {kargs if kargs else 'sem args'}
+        """)
+    if rnd < .2:
+        raise ConnectionAbortedError('Conexão foi finalizada')
+    elif rnd < .4:
+        raise ConnectionRefusedError('Conexão foi recusada')
+    elif rnd < .6:
+        raise TimeoutError('Tempo de espera excedido')
+    else:
+        return "OK!"
+# %%
+test_func(42, 51, nome="victor")
 # %%
